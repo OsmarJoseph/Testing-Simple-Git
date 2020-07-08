c700a034f747eb13302ecf469583b052db3725bc
Osmar Joseph <osmarjoseph@MacBook-de-Kai.local>
2020-07-08 13:56:09 -0300

    simple git works

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import simpleGit from 'simple-git';
 import fs from 'fs';
 fs.writeFileSync("teste", "123")
-// const git = simpleGit();
-git.stash()
+const git = simpleGit();
+git.add("./*")
+git.commit("simple git works")
